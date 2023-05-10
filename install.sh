f3cae5e826dbcb502743025231c6f8dfa36d56e2
hexis <hexis@2603-6010-9b02-d506-0000-0000-0000-18f7.res6.spectrum.com>
2023-05-10 12:16:28 -0400

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/sh
 
-cd ~/ && git clone https://github.com/hexisXz/setwall.git && cd setwall && mkdir ~/.config/themes && mv themes.sh ~/.config/themes/ && sudo mv setwall /bin/ && echo "source ~/.config/themes/themes.sh" >> ~/."$0"rc && sudo rm -r ~/setwall
+cd ~/ && git clone https://github.com/hexisXz/setwall.git && cd setwall && mkdir ~/.config/themes && mv themes.sh ~/.config/themes/ && mv wallpapers ~/.config/ && sudo mv setwall /bin/ && echo "source ~/.config/themes/themes.sh" >> ~/."$0"rc && sudo rm -r ~/setwall
 
 
